3af8d9cdb5aa7c5c51de60ee6eb1ab029e9edc8e
许源铎 <buxiang@tianxiadatong.net.cn>
2021-02-12 17:28:03 +0800

    初始化

--- pc/.eslintrc.js
+++ pc/.eslintrc.js
@@ -4,12 +4,12 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/essential',
     '@vue/standard'
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
+  // parserOptions: {
+  //   parser: 'babel-eslint'    这里是配置要不要使用eslint规范，因太麻烦所以注释掉
+  // },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
