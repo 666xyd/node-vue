4fe0d4e7ed9f3f476834112d4bea5b5b02c50750
许源铎 <buxiang@tianxiadatong.net.cn>
2021-02-23 15:34:59 +0800

    pc

---
+++ app/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="header bg-primary flex flex-center">
+        <template v-for="(item, index) in headerList">
+            <div :key="index" class="header-item fc-white">
+                {{item.title}}
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        data(){
+            return {
+                headerList: [
+                    {
+                        title: '首页',
+                        url: ''
+                    },
+                    {
+                        title: '攻略中心',
+                        url: '',
+                    },
+                    {
+                        title: '赛事中心',
+                        url: '',
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .header{
+        height: 3.692rem;
+        line-height: 3.692rem;
+    }
+
+    .header-item{
+        width: 33.3%;
+        text-align: center;
+    }
+</style>
---
+++ app/src/main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="main">
+        <Header></Header>
+    </div>
+</template>
+
+<script>
+    import './app.public.css';
+    import Header from "./components/Header";
+    export default {
+        name: "wap",
+        components: {
+            Header
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
